fdf0e4907ced7d9021a8b852cab6712c61b56043
timkerekes <timothy.kerekes@gmail.com>
2022-09-17 20:31:47 -0600

    learn-terraform-aws-instance

--- learn-terraform-provider-versioning/main.tf
+++ learn-terraform-provider-versioning/main.tf
@@ -18,3 +18,12 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.sample.id
   acl    = "public-read"
 }
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
